e361fb5af4a31c40769d91f3e197005429556752
Your_Name <joseph161216@gmail.com>
2020-04-22 20:11:55 -0700

    add patch and delete operations

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,6 +1,20 @@
 const router = require("express").Router();
 const Post = require("../models/postModel");
 
+router.get("/", async (req, res) => {
+  const posts = await Post.find();
+  res.json(posts);
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    res.json(post);
+  } catch (err) {
+    console.error(err);
+  }
+});
+
 router.post("/", async (req, res) => {
   // retrieve the data from the req
   const { title, createdAt, tags, html } = req.body;
@@ -19,18 +33,45 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.get("/", async (req, res) => {
-  const posts = await Post.find();
-  res.json(posts);
+//patch req
+router.patch("/:id", (req, res) => {
+  const post = {
+    title: req.body.title,
+    createdAt: Date.now(),
+    tags: req.body.tags,
+    html: req.body.html
+  }
+  Post.findByIdAndUpdate({_id: req.params.id}, post, {useFindAndModify: false}, (err, post) => {
+    if(err) {
+      console.error(err)
+      res.json({
+        error: err
+      })
+    }
+    res.json(post)
+  })
+})
+
+//put req
+// router.put("/:id", (req, res) => {
+//   Post.findByIdAndUpdate(req.params.id, { $set: req.body }, (err, post) => {
+//     if(err) return next(err);
+//     res.send("Post updated.")
+//   })
+// })
+
+//delete post
+router.delete("/:id", (req, res) => {
+  Post.findById(req.params.id, (err, post) => {
+    post.remove((err) => {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.status(204).send("removed");
+      }
+    });
+  });
 });
 
-router.get("/:id", async (req, res) => {
-  try {
-    const post = await Post.findById(req.params.id);
-    res.json(post);
-  } catch (err) {
-    console.error(err);
-  }
-})
 
 module.exports = router;
