f551d52fc3bb96d842f84b1dd06d3cc13771d58e
Your_Name <joseph161216@gmail.com>
2020-04-28 17:40:42 -0700

    add data fetching from server

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,23 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 import Post from "./components/post"
 import NavbarComponent from "./components/navBar";
 
 function App() {
-  const [posts, setPosts] = useState([
-      {
-        title: "First Post",
-        createdAt: "12/12/2020",
-        tags: ["html", "CSS"],
-        html: "Hi There!",
-      },
-      {
-        title: "First Post",
-        createdAt: "12/12/2020",
-        tags: ["html", "CSS"],
-        html: "Hi There!",
-      },
-    ]
-  );
+  const [posts, setPosts] = useState([]);
+  // TODO: Implement laoding when refactor
+  // const [loading, setLoading] = React.useState("false");
+
+  // ! ordering of the return may not be right.
+  // * Is there a way to return without the function call?
+  useEffect(() => {
+    async function fetchPostList() {
+      try {
+        // setLoading("true");
+        const response = await fetch("http://localhost:5000/posts");
+        const json = await response.json()
+        // json.map(post => {
+        //   console.log(post.title)
+        // })
+        setPosts(json.map(post => {
+          return post
+        }))
+      } catch (err) {
+        console.error(err)
+        // setLoading("null");
+      }
+    };
+    fetchPostList();
+    return [posts];
+  }, []);
+
 
   return (
     <div>
